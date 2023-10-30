e35fc16001801c941af925360ac854fb597cdaf1
iamdumonde <martinaboumonde@gmail.com>
2023-10-30 19:09:58 +0100

    essai de l'exo question2

---
+++ question2.php
@@ -0,0 +1,11 @@
+<?php
+
+$str = <<<Text
+Le numéro de téléphone (123) 456-7890 de certains clients
+Le numéro de téléphone 555-1234 de certains clients
+Le numéro de téléphone (555) 555-5555 de certains clients
+Le numéro de téléphone 800-867-5309 de certains clients
+Le numéro de téléphone (123) 555-6789 de certains clients
+Le numéro de téléphone 123-456-7890 123 456 7890 123.456.7890 de certains clients
+Text;
+
